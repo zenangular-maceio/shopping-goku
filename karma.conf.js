c677393c013a86203c7b7e632d55d1a6d8473291
Gustavo Costa <gustavo.costa.w@gmail.com>
2014-08-14 20:36:24 -0300

    first commit

---
+++ karma.conf.js
@@ -0,0 +1,26 @@
+module.exports = {
+  frameworks: ['jasmine'],
+
+  plugins: ['karma-jasmine', 'karma-chrome-launcher', 'karma-nyan-reporter'],
+
+  reporters: ['nyan'],
+
+  browsers: ['Chrome'],
+
+  files: [
+    'app/bower_components/angular/angular.js',
+    'app/bower_components/angular-mocks/angular-mocks.js',
+
+    'app/app.js',
+    'app/!(bower_components)/*Module.js',
+    'app/!(bower_components)/*Directive.js',
+    'app/!(bower_components)/*Controller.js',
+    'app/!(bower_components)/*Factory.js',
+    'app/!(bower_components)/*Service.js',
+    'app/!(bower_components)/*Provider.js',
+    'app/!(bower_components)/*Filter.js',
+
+    'test/unit/**/*Spec.js'
+  ]
+};
+
